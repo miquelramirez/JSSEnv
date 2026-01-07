ec219f74d7c50203d7801b8fd3ff03cc93f973c8
Miquel Ramirez <miquel.ramirez@praetorian-aeronautics.com>
2026-01-07 16:48:34 +1100

    Random scheduler
    - Working on the random scheduler

---
+++ examples/random_scheduler.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import numpy as np
+
+import gymnasium as gym
+import JSSEnv
+import JSSEnv.envs.instances as jsslib
+
+def main() -> None:
+    """
+    Main function
+    """
+
+    env = gym.make('jss-v1',
+                   env_config=dict(instance_path=jsslib.get_path() / "ta80",
+                                   always_allow_noop=True))
+
+    obs_0, info_0 = env.reset(seed=42)
+    done: bool = False
+    policy_cost_obs: float = 0
+
+    while not done:
+        # Get legal actions
+        legal_actions = [a for a, applicable in enumerate(obs_0.get('action_mask')) if applicable]
+        if len(legal_actions) == 0:
+            raise RuntimeError('No op should always be a legal action')
+        # Get a random legal action
+        action = np.random.choice(legal_actions, 1)[0]
+
+        obs_t, r_t, done_t, trunc_t, info_t = env.step(action)
+        policy_cost_obs += r_t
+
+if __name__ == '__main__':
+    main()
