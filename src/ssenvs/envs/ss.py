d87ae509e2da18dbfc370d3f98156ea8ba0e3d38
Miquel Ramirez <miquel.ramirez@praetorian-aeronautics.com>
2026-04-20 15:29:02 +1000

    Super arm and reward format
     - Changed super arm and reward formats

--- src/ssenvs/envs/ss.py
+++ src/ssenvs/envs/ss.py
@@ -49,6 +49,11 @@ class PredictionEnv(gym.Env):
 
         self.feedback: dict[tuple[int, int], float] = {}
 
+        self._app_mask: np.ndarray = np.zeros((0,0))
+        self._known_jobs: set[int] = set()
+        self._just_completed: set[int] = set()
+        self._just_failed: set[int] = set()
+        self.n_t: int = 0
 
 
     def reset(self, seed: int | None = None,
@@ -74,6 +79,14 @@ class PredictionEnv(gym.Env):
         self.t_max = options['t_max']
         self.deadlines = options['deadlines']
 
+        self._known_jobs = self.completed | self.pending | self.failed | set([j for j, _ in self.working])
+
+        self.n_t = max(self._known_jobs)
+
+        self._app_mask = np.zeros((self.instance.machines, self.n_t + 1), dtype=np.bool)
+        self._just_completed: set[int] = set()
+        self._just_failed: set[int] = set()
+
         # put all released jobs out
 
         self.trace = [self._get_obs()]
@@ -81,19 +94,28 @@ class PredictionEnv(gym.Env):
 
         return self.trace[-1], self._get_info()
 
-    def step(self, action: list[tuple[int, int]]) -> tuple[ObservationSpaceType, InfoType, float, bool, bool]:
+    def step(self, action: np.ndarray) -> tuple[ObservationSpaceType, InfoType, float, bool, bool]:
         """
         Steps the environment
         """
 
-        for j, i in action:
-            if j not in self.pending:
-                raise ValueError(f"Invalid assignment: ({j}, {i}): Job {j} not in pending.")
-            if i not in self.idle:
-                raise ValueError(f"Invalid assignment: ({j}, {i}): Machine {i} not in idle.")
+        assert action.shape[0] == self.instance.machines
+        assert action.shape[1] == self.n_t + 1
+
+        m: int = self.instance.machines
+        n: int = self.n_t
+
+        for i in range(m):
+            for j in range(n):
+                if action[i, j]:
+                    if j not in self.pending:
+                        raise ValueError(f"Invalid assignment: ({j}, {i}): Job {j} not in pending.")
+                    if i not in self.idle:
+                        raise ValueError(f"Invalid assignment: ({j}, {i}): Machine {i} not in idle.")
 
         self.current_time_step += 1
-
+        self._just_completed: set[int] = set()
+        self._just_failed: set[int] = set()
         if self.current_time_step == self.t_max:
             logging.debug(f"Done with rollout")
             self.trace.append(self._get_obs())
@@ -108,6 +130,7 @@ class PredictionEnv(gym.Env):
             logging.debug(f"Checking status of job {j} being processed by machine {i}")
             if self.current_time_step > self.deadlines[j]:
                 self.failed.add(j)
+                self._just_failed.add(j)
                 self.idle.add(i)
                 self.elapsed[j] = 0
                 logging.debug(f"Job {j} FAILS: current time: {self.current_time_step}, deadline: {self.deadlines[j]}")
@@ -115,6 +138,7 @@ class PredictionEnv(gym.Env):
                 if self.elapsed[j] > self.instance.process_times[j, i]:
                     logging.debug(f"Job {j} COMPLETED at machine {i}: current time: {self.current_time_step}, deadline: {self.elapsed[j]}")
                     self.completed.add(j)
+                    self._just_completed.add(j)
                     self.idle.add(i)
                     self.elapsed[j] = 0
                     self.feedback[(j, i)] = 1.0 # note we are assuming unit weights
@@ -131,18 +155,21 @@ class PredictionEnv(gym.Env):
                         logging.debug(f"Job {j} ON TRACK at machine {i}: current time: {self.current_time_step}, deadline: {self.deadlines[j]}")
                         self.elapsed[j] += 1
                         next_working.add((j, i))
-        for j, i in action:
-            v_ji = self.np_random.random()
-            q_ji = self.instance.process_probs[j, i]
-            logging.debug(f"Test for job {j} successfully starts processing at machine {i}: disturbance: {v_ji}, probability: {q_ji}")
-            if v_ji > q_ji:
-                logging.debug(f"Job {j} ABORTED upon starting at machine {i}: current time: {self.current_time_step}, deadline: {self.deadlines[j]}")
-            else:
-                next_working.add((j, i))
-                self.elapsed[j] = 1
-                self.idle.remove(i)
-                self.pending.remove(j)
-                logging.debug(f"Adding new entry to working set: job={j}, machine={i}")
+        for i in range(m):
+            for j in range(n):
+                if not action[i, j]:
+                    continue
+                v_ji = self.np_random.random()
+                q_ji = self.instance.process_probs[j, i]
+                logging.debug(f"Test for job {j} successfully starts processing at machine {i}: disturbance: {v_ji}, probability: {q_ji}")
+                if v_ji > q_ji:
+                    logging.debug(f"Job {j} ABORTED upon starting at machine {i}: current time: {self.current_time_step}, deadline: {self.deadlines[j]}")
+                else:
+                    next_working.add((j, i))
+                    self.elapsed[j] = 1
+                    self.idle.remove(i)
+                    self.pending.remove(j)
+                    logging.debug(f"Adding new entry to working set: job={j}, machine={i}")
         logging.debug(f"Next working set: {next_working}")
         logging.debug(f"Elapsed time: {self.elapsed}")
         self.working = next_working
@@ -152,6 +179,7 @@ class PredictionEnv(gym.Env):
         for j in self.pending:
             if self.current_time_step > self.deadlines[j]:
                 self.failed.add(j)
+                self._just_failed.add(j)
                 self.elapsed[j] = 0
             else:
                 next_pending.add(j)
@@ -160,14 +188,16 @@ class PredictionEnv(gym.Env):
         self.trace.append(self._get_obs())
         return self.trace[-1], self._get_info(), 0.0, False, False
 
-    def _get_reward(self) -> float:
+    def _get_reward(self) -> list[tuple[int, float]]:
         """
         Returns reward
         """
-        completed_weights: int = 0
-        for j in self.completed:
-            completed_weights += self.instance.job_weights[j]
-        return completed_weights
+        rewards: list[tuple[int, float]] = []
+        for j in self._just_completed:
+            rewards.append((j, self.instance.job_weights[j]))
+        for j in self._just_failed:
+            rewards.append((j, -self.instance.job_weights[j]))
+        return rewards
 
     def _get_obs(self) -> ObservationSpaceType:
         """
@@ -184,16 +214,17 @@ class PredictionEnv(gym.Env):
                     jobs_deadlines={j: self.deadlines[j] for j in self.pending},)
 
     def _get_info(self) -> InfoType:
+        self._update_available_arms()
         return dict(t=self.current_time_step,
                     feedback=self.feedback,
-                    arms=self._calc_available_arms(),
+                    arms=self._app_mask.copy(),
                     elapsed=self.elapsed,)
 
-    def _calc_available_arms(self) -> list[tuple[int, int]]:
-        applicable: list[tuple[int, int]] = []
+    def _update_available_arms(self) -> None:
+        self._app_mask = np.zeros_like(self._app_mask, dtype=bool)
+        m: int = self._app_mask.shape[0]
         for j in self.pending:
             for i in self.idle:
                 if self.current_time_step + self.instance.process_times[j, i] < self.deadlines[j]:
-                    applicable.append((j, i))
-
-        return applicable
+                    self._app_mask[i, j] = True
+        self._app_mask[:, self.n_t + 1] = True
