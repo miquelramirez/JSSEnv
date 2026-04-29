3043e3daf3940a5689bb28c1621dd021f89b114a
Jonathan Thomas <jonathan.thomas@praetorian-aeronautics.com>
2026-04-29 21:41:52 +1000

    add in reward reporting for each time step

--- src/ssenvs/envs/poss.py
+++ src/ssenvs/envs/poss.py
@@ -180,7 +180,7 @@ class StochasticEnv(gym.Env):
         self.pending = next_pending
 
         self.trace.append(self._get_obs())
-        return self.trace[-1], self._get_info(), 0.0, False, False
+        return self.trace[-1], self._get_info(), self._get_reward(), False, False
 
     def _get_reward(self) -> float:
         """
